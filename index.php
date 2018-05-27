2be1006aff00ff46a0b5ec1bd28898226dde7b7e
iespino <iespino69@gmail.com>
2018-05-27 18:37:12 -0500

    27/05/2018

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
-data = file_get_contents("datos.json");
-$products = json_decode($data, true);
 
-foreach ($products as $product) {
-    print_r($product);
+$datos = file_get_contents("datos.json");
+$json = json_decode($datos, true);
+
+
+echo '<table style="width:100%" style= "border: 1px solid black">
+    <tr>
+         <th>Id</th>
+         <th>User</th> 
+         <th>email</th>
+    </tr>      
+';
+foreach ($json as $user) 
+{
+    echo '<tr>
+              <th>'.$user['id'].'</th>
+              <th>'.$user['usuario'].'</th>
+              <th>'.$user['email'].'</th>
+          </tr>';
+   // print_r ($user);
 }
-
-
+echo '</table>';
 ?>
